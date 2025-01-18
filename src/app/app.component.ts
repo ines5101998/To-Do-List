5d6c562dbce953ae535904b3815c8af0bbeaba4c
Ines <benkhaledines510@gmail.com>
2025-01-18 12:54:28 +0100

    the static view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodolistComponent } from './todolist/todolist.component';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, TodolistComponent],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
