edb7895b780177a7d1a63f5d4d22abbff8e748a6
Ines <benkhaledines510@gmail.com>
2025-01-18 14:29:30 +0100

    it works !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodolistComponent } from './todolist/todolist.component';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TodolistComponent],
+  imports: [RouterOutlet, TodolistComponent, FormsModule],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todolist',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './todolist.component.html',
   styleUrl: './todolist.component.css'
 })
-export class TodolistComponent {
+export class TodolistComponent implements OnInit {
+taskArray=[{taskName:'Brush teeth', isCompleted: false}];
+constructor(){}
+
+ngOnInit(): void {
+  
+}
+
+  onSubmit(form : NgForm){
+
+    this.taskArray.push({
+     taskName : form.controls['task'].value,
+     isCompleted : false
+
+    })
+
+form.reset();
+
+
+  }
+
+
+
+  onDelete(index: number){
+
+    this.taskArray.splice(index, 1);
+
+  }
+
+  onCheck(index: number){
+
+    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted ;
+
+  }
 
 }
